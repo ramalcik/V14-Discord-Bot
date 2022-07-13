1cbf53e50280b88ebd7fd34149963d9ae0a0445f
Ramal? <71768732+ramalcik@users.noreply.github.com>
2022-07-14 02:25:03 +0400

    Delete Log directory

--- Log/models/yetkilisay.js
+++
@@ -1,9 +0,0 @@
-let mongoose = require("mongoose");
-
-let yetkilisayDB = mongoose.Schema({
-    userID: String,
-    Amount: Number,
-    Info: Object
-});
-
-module.exports = mongoose.model("yetkilisay", yetkilisayDB);
