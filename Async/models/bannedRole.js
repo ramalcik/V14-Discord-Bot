a3bd878d30d42e2286e9bb2857f2f5a49542eebd
Ramal? <71768732+ramalcik@users.noreply.github.com>
2022-07-14 02:24:16 +0400

    Delete Async directory

--- Async/models/bannedRole.js
+++
@@ -1,8 +0,0 @@
-let mongoose = require("mongoose");
-let Schema = mongoose.Schema;
-
-let banRole = Schema({
-    BanRole: {type: Array, default: []}
-});
-
-module.exports = mongoose.model("banrol", banRole);
