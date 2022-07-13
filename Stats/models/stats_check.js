4c66f4407486eb8c5807ae4bfeff55da36103579
Ramal? <71768732+ramalcik@users.noreply.github.com>
2022-07-14 02:25:34 +0400

    Delete Stats directory

--- Stats/models/stats_check.js
+++
@@ -1,9 +0,0 @@
-let mongoose = require("mongoose");
-let Schema = mongoose.Schema;
-
-let kontrol = Schema({
-    guildID: String,
-    haftalik: Number,
-});
-
-module.exports = mongoose.model("kontrol", kontrol);
